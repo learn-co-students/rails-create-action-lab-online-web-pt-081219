95aff3369f3deb1da382e8db58c1d89e249fe084
saramccombs <theSaraMcCombs@gmail.com>
2020-01-09 17:19:14 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+sara = Student.create(first_name: "Sara", last_name: "McCombs")
